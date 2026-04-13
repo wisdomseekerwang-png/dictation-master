810e4fe51baf0dd5731b9b1ca148d761f2c7df3e
XIONG Yuhao <yuhaoxiong@cmi.local>
2026-04-13 15:29:46 +0800

    fix: update build config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
